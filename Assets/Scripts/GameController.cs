3017800f8f662305217579b987c59790a9e976bf
kossam001 <kossam001@gmail.com>
2021-01-18 13:28:09 -0500

    Added a script to use Graphic Raycaster
    Got the code from a previous project, which got it from Unity docs.  Instead of using Buttons for tiles, I am using a Graphics Raycaster to retrieve UI GameObjects and performing operations on them.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GameController : MonoBehaviour
+{
+    // Graphic Raycaster code from https://docs.unity3d.com/2017.3/Documentation/ScriptReference/UI.GraphicRaycaster.Raycast.html
+    GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    EventSystem m_EventSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the Raycaster from the GameObject (the Canvas)
+        m_Raycaster = GetComponent<GraphicRaycaster>();
+        //Fetch the Event System from the Scene
+        m_EventSystem = GetComponent<EventSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if the left Mouse button is clicked
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // Code from https://forum.unity.com/threads/graphicraycaster-raycast-on-nested-canvases.603436/
+            PointerEventData m_PointerEventData = new PointerEventData(m_EventSystem);
+            m_PointerEventData.position = Input.mousePosition;
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(m_PointerEventData, results);
+
+            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+            foreach (RaycastResult result in results)
+            {
+                Debug.Log(result.gameObject.name);
+            }
+        }
+
+    }
+}
