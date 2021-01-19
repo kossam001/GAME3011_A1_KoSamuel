94f5e26a8a0afa2146b98d5769685c5a7389ea2a
kossam001 <kossam001@gmail.com>
2021-01-19 10:49:06 -0500

    Setting up grid layout
    GameManager instantiates Resource Tiles

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public GameObject uiResourceGrid;
+
+    public int gridSize;
+
+    List<List<Resource>> resourceGrid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setup();
+    }
+
+    private void Setup()
+    {
+        resourceGrid = new List<List<Resource>>();
+
+        // Setup Grid
+        for (int row = 0; row < gridSize; row++)
+        {
+            List<Resource> resourceRow = new List<Resource>();
+
+            for (int col = 0; col < gridSize; col++)
+            {
+                GameObject resourceTile = Instantiate(tilePrefab);
+                resourceTile.transform.SetParent(uiResourceGrid.transform);
+
+                resourceRow.Add(resourceTile.GetComponent<Resource>());
+            }
+
+            resourceGrid.Add(resourceRow);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
